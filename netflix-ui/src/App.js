af35534c4298957908961346565cc8414547a744
pinkjagnie <olga.krzysztofik@gmail.com>
2022-09-27 21:49:31 +0200

    Setup pages

--- netflix-ui/src/App.js
+++ netflix-ui/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Netflix from "./pages/Netflix";
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Netflix />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  )
+};
 
 export default App;
---
+++ netflix-ui/src/pages/Netflix.js
@@ -0,0 +1,7 @@
+const Netflix = () => {
+  return(
+    <div>Netflix</div>
+  )
+};
+
+export default Netflix;
