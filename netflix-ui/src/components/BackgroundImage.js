0620b0a48e7b1268c42d26932c19dd47e1e548bb
pinkjagnie <olga.krzysztofik@gmail.com>
2022-09-27 22:13:29 +0200

    Setup Signup page, added firebase-config

---
+++ netflix-ui/src/components/BackgroundImage.js
@@ -0,0 +1,7 @@
+const BackgroundImage = () => {
+  return(
+    <div>bgc img</div>
+  )
+};
+
+export default BackgroundImage;
--- netflix-ui/src/pages/Signup.js
+++ netflix-ui/src/pages/Signup.js
@@ -1,7 +1,30 @@
+import BackgroundImage from "../components/BackgroundImage";
+import Header from "../components/Header";
+
+import styled from "styled-components";
+
 const Signup = () => {
   return(
-    <div>Signup</div>
+    <Container>
+      <BackgroundImage />
+      <Header />
+      <div className="body flex column a-center j-center">
+        <div className="text flex column">
+          <h1>Unlimited movies, TV shows and more.</h1>
+          <h4>Watch anywhere. Cancel anytime.</h4>
+          <h6>Ready to watch? Enter your email to create or restart membership.</h6>
+        </div>
+        <div className="form">
+          <input type="email" placeholder="Email address" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Get Started</button>
+        </div>
+        <button>Log In</button>
+      </div>
+    </Container>
   )
 };
 
 export default Signup;
+
+const Container = styled.div``;
