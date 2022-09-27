d9e6cfbaa85826022aaa4ff980aff800a09c817a
pinkjagnie <olga.krzysztofik@gmail.com>
2022-09-27 23:17:37 +0200

    Handling SignUp

--- netflix-ui/src/pages/Signup.js
+++ netflix-ui/src/pages/Signup.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 import BackgroundImage from "../components/BackgroundImage";
 import Header from "../components/Header";
 
 import styled from "styled-components";
 
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const signUpHandler = () => {
+    console.log(formValues)
+  };
+
   return(
-    <Container>
+    <Container showPassword={showPassword}>
       <BackgroundImage />
       <div className="content">
         <Header login/>
@@ -16,11 +28,19 @@ const Signup = () => {
             <h6>Ready to watch? Enter your email to create or restart membership.</h6>
           </div>
           <div className="form">
-            <input type="email" placeholder="Email address" name="email" />
-            <input type="password" placeholder="Password" name="password" />
-            <button>Get Started</button>
+            <input type="email" placeholder="Email address" name="email" value={formValues.email} onChange={(event) => setFormValues({
+              ...formValues, 
+              [event.target.name]: event.target.value
+              })
+            } />
+            {showPassword && <input type="password" placeholder="Password" name="password" value={formValues.password} onChange={(event) => setFormValues({
+                ...formValues,
+                [event.target.name]: event.target.value
+              })
+            } />}
+            {!showPassword && <button onClick={() => setShowPassword(true)}>Get Started</button>}
           </div>
-          <button>Log In</button>
+          <button onClick={signUpHandler}>Sign Up</button>
         </div>
       </div>
     </Container>
@@ -40,5 +60,51 @@ const Container = styled.div`
     width: 100vw;
     display: grid;
     grid-template-rows: 15vh 85vh;
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        h1 {
+          padding: 0 25rem;
+        }
+      }
+      .form {
+        display: grid;
+        grid-template-columns: ${({ showPassword }) =>
+          showPassword ? "1fr 1fr" : "2fr 1fr"};
+        width: 60%;
+        input {
+          color: black;
+          border: none;
+          padding: 1.5rem;
+          font-size: 1.2rem;
+          border: 1px solid black;
+          &:focus {
+            outline: none;
+          }
+        }
+        button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          font-weight: bolder;
+          font-size: 1.05rem;
+        }
+      }
+      button {
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        border-radius: 0.2rem;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
   }
 `;
